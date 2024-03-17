379761806be0590f1cb7a0ea67803a35be6725fc
Rohit <rm831799@gmail.com>
2024-03-18 01:59:10 +0530

    bug fixing

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import json;
 from difflib import get_close_matches;
+from typing import List
 
 def load_knowledge_base(file_path: str) -> dict:
     with open(file_path, 'r') as file:
@@ -10,8 +11,8 @@ def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
-def find_knowledge_match(user_question: str, questions: list[str]) -> str :
-    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+def find_knowledge_match(user_question: str, questions: List[str]) -> str :
+    matches: List = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
 def get_answer_for_question(question: str, knowledge_base: dict) -> str :
